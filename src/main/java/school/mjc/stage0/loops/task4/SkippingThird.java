714e6c33bdf4ca09c9305fefffbacc1fa31a06cd
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-12-13 11:07:33 +0500

    commit1 loops task4

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.loops.task4;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
         if (lastPrinted==0||lastPrinted<0){
-            System.out.println("");
+            System.out.print("");
         }else {
-            for (int i = 1; i < 7; i++) {
+            for (int i = 1; i <= 7; i++) {
                 if (i%3==0){
                     continue;
                 }
